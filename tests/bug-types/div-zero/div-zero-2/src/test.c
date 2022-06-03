01f525ef9ef20e8d7b6f3fa420ebfb6d619b6fe4
Your Name <you@example.com>
2022-06-03 15:25:20 +0800

    update div-zero test cases

---
+++ tests/bug-types/div-zero/div-zero-2/src/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int main(int argc, char *argv[]) {
+  int x = atoi(argv[1]);
+  int res, z;
+  if (__trident_choice("L9", "bool", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0))  {
+      return -1;
+  }
+
+  if (x > 5)
+    z = x - 7;
+  else
+    z = x + 2;
+
+  TRIDENT_OUTPUT("obs", "i32", z);
+  res = 1000 / z;
+  return 0;
+}
+
+
+
+
+
+
+
+
--- tests/bug-types/div-zero/div-zero-4/components/constant_b.smt2
+++
@@ -1,3 +0,0 @@
-(declare-const const_b (_ BitVec 32))
-(declare-const rreturn (_ BitVec 32))
-(assert (= rreturn const_b))
