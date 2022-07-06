dd603fa111ae01df92a9aac36f5c4d20e9d6c2b1
ChrisTimperley <christimperley@googlemail.com>
2022-07-06 14:38:47 -0400

    srcrepair: added build directory to .gitignore

---
+++ source-repair/include/crashrepairfix/SourceLocation.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+#include "Utils.h"
+
+namespace crashrepairfix {
+
+class SourceLocation {
+public:
+  std::string file;
+  size_t line;
+  size_t column;
+
+  SourceLocation(
+    std::string const &file,
+    size_t line,
+    size_t column
+  ) : file(file), line(line), column(column) {}
+
+  bool operator==(const SourceLocation &rhs) const {
+    return file == rhs.file && line == rhs.line && column == rhs.column;
+  }
+
+  bool operator<(const SourceLocation &rhs) const {
+    return file < rhs.file && line < rhs.line && column < rhs.column;
+  }
+
+  static SourceLocation fromString(std::string const &str) {
+    auto parts = split(str, ':');
+    assert (parts.size() == 3);
+
+    auto file = parts[0];
+    auto line = std::stoi(parts[1]);
+    auto column = std::stoi(parts[2]);
+    return SourceLocation(file, line, column);
+  }
+};
+
+}
