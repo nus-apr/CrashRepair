5d14f0c5047a0dda45377c8048fc4e789ce974fb
ChrisTimperley <christimperley@googlemail.com>
2022-07-06 15:13:33 -0400

    srcrepair: added FixLocation class

---
+++ source-repair/include/crashrepairfix/FixLocation.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include "SourceLocation.h"
+
+namespace crashrepairfix {
+
+class FixLocation {
+private:
+  SourceLocation sourceLocation;
+
+public:
+  FixLocation(
+    SourceLocation &sourceLocation
+  ) : sourceLocation(sourceLocation) {}
+
+  static FixLocation fromJSON(nlohmann::json j) {
+    SourceLocation location = SourceLocation::fromString(j["location"]);
+    return FixLocation(location);
+  }
+};
+
+}
