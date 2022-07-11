d8d729c0c2ec6ebcdd7a34acd7474c5850649b36
ChrisTimperley <christimperley@googlemail.com>
2022-07-11 15:43:34 -0400

    srcrepair: added yesOrNo and isInsideLoop helpers

--- source-repair/include/crashrepairfix/Utils.h
+++ source-repair/include/crashrepairfix/Utils.h
@@ -8,6 +8,8 @@
 #include <clang/AST/Stmt.h>
 #include <clang/Basic/SourceManager.h>
 #include <clang/Lex/Lexer.h>
+#include <clang/AST/ASTTypeTraits.h>
+#include <clang/AST/ParentMapContext.h>
 
 namespace crashrepairfix {
 
@@ -16,5 +18,10 @@ std::vector<std::string> split(const std::string &s, char delim);
 std::string getSource(clang::Stmt const *stmt, clang::SourceManager const &sourceManager);
 
 std::string getSource(clang::Stmt const *stmt, clang::ASTContext const &context);
+
+std::string yesOrNo(bool status);
+
+bool isInsideLoop(clang::Stmt const *stmt, clang::ASTContext &context);
+bool isInsideLoop(clang::DynTypedNode const &node, clang::ASTContext &context);
 
 }
--- source-repair/src/crashrepairfix/Utils.cpp
+++ source-repair/src/crashrepairfix/Utils.cpp
@@ -27,4 +27,30 @@ std::string getSource(clang::Stmt const *stmt, clang::ASTContext const &context)
   return getSource(stmt, context.getSourceManager());
 }
 
+std::string yesOrNo(bool status) {
+  if (status) {
+    return "yes";
+  } else {
+    return "no";
+  }
+}
+
+bool isInsideLoop(clang::DynTypedNode const &node, clang::ASTContext &context) {
+  for (auto const parent : context.getParents(node)) {
+    std::string nodeKind = parent.getNodeKind().asStringRef().str();
+    if (nodeKind == "WhileStmt" || nodeKind == "ForStmt") { 
+      return true;
+    }
+    if (isInsideLoop(parent, context)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isInsideLoop(clang::Stmt const *stmt, clang::ASTContext &context) {
+  auto node = clang::DynTypedNode::create(*stmt);
+  return isInsideLoop(node, context);
+}
+
 }
