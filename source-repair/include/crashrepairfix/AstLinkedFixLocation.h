0d04b6abf3ee5d53cbbd21b93a4ea354a9a70389
ChrisTimperley <christimperley@googlemail.com>
2022-07-11 16:58:49 -0400

    srcrepair: added AstLinkedFixLocation

---
+++ source-repair/include/crashrepairfix/AstLinkedFixLocation.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "FixLocation.h"
+#include "Utils.h"
+
+namespace crashrepairfix {
+
+class AstLinkedFixLocation {
+private:
+  FixLocation const &fixLocation;
+  clang::Stmt *stmt;
+  clang::ASTContext &context;
+
+  AstLinkedFixLocation(
+    FixLocation const &fixLocation,
+    clang::Stmt *stmt,
+    clang::ASTContext &context
+  ) : fixLocation(fixLocation), stmt(stmt), context(context) {}
+
+public:
+  static AstLinkedFixLocation create(
+    FixLocation const &fixLocation,
+    clang::Stmt *stmt,
+    clang::ASTContext &context
+  ) {
+    return AstLinkedFixLocation(
+      fixLocation,
+      stmt,
+      context
+    );
+  }
+
+  std::string getSource() const {
+    return crashrepairfix::getSource(stmt, context);
+  }
+
+  FixLocation const & getFixLocation() const {
+    return fixLocation;
+  }
+
+  SourceLocation const & getLocation() const {
+    return fixLocation.getLocation();
+  }
+};
+
+}
+
