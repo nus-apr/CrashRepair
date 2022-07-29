53b2f551e789649cc1a6ead0d1a40dda061c9511
Yannic Noller <yannic.noller@acm.org>
2022-07-29 16:57:01 +0800

    removed duplicated function definition

--- lib/crepair_proxy.c
+++ lib/crepair_proxy.c
@@ -10,14 +10,6 @@ float fabs_trident(float a){
   return -a;
 }
 
-float fabs_trident(float a){
-
-  if (a > 0){
-     return a;
-  }
-  return -a;
-}
-
 float rint_trident(float a){
 
   return (int) a;
