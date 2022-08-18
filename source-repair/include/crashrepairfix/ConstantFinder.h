9bdfde8baa8b44152cc607edc00385c3d3482c36
ChrisTimperley <christimperley@googlemail.com>
2022-08-18 15:10:33 -0400

    repair: added ConstantFinder helper

---
+++ source-repair/include/crashrepairfix/ConstantFinder.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <set>
+#include <variant>
+
+#include <clang/AST/Expr.h>
+#include <clang/AST/LexicallyOrderedRecursiveASTVisitor.h>
+
+#include <llvm/ADT/APInt.h>
+
+#include <spdlog/spdlog.h>
+
+#include "SourceLocation.h"
+
+namespace crashrepairfix {
+
+class ConstantFinder
+  : public clang::LexicallyOrderedRecursiveASTVisitor<ConstantFinder> {
+public:
+  static std::vector<std::variant<llvm::APInt, llvm::APFloat>> find(
+    clang::ASTContext &context,
+    clang::TranslationUnitDecl const *translationUnit,
+    std::string const &restrictToFile
+  ) {
+    ConstantFinder finder(context, restrictToFile);
+    finder.TraverseDecl(const_cast<clang::TranslationUnitDecl*>(translationUnit));
+    return finder.result;
+  }
+
+  static std::vector<llvm::APInt> findIntegers(
+    clang::ASTContext &context,
+    clang::TranslationUnitDecl const *translationUnit,
+    std::string const &restrictToFile
+  ) {
+    std::vector<llvm::APInt> results;
+    auto constants = find(context, translationUnit, restrictToFile);
+    for (auto &constant : constants) {
+      if (std::holds_alternative<llvm::APInt>(constant)) {
+        results.push_back(std::get<llvm::APInt>(constant));
+      }
+    }
+    return results;
+  }
+
+  static std::vector<llvm::APFloat> findReals(
+    clang::ASTContext &context,
+    clang::TranslationUnitDecl const *translationUnit,
+    std::string const &restrictToFile
+  ) {
+    std::vector<llvm::APFloat> results;
+    auto constants = find(context, translationUnit, restrictToFile);
+    for (auto &constant : constants) {
+      if (std::holds_alternative<llvm::APFloat>(constant)) {
+        results.push_back(std::get<llvm::APFloat>(constant));
+      }
+    }
+    return results;
+  }
+
+  clang::ASTContext &context;
+  std::string restrictToFile;
+  clang::SourceManager &sourceManager;
+  std::unordered_map<std::string, std::string> relativeToAbsoluteFilenames;
+  std::vector<std::variant<llvm::APInt, llvm::APFloat>> result;
+
+  explicit ConstantFinder(clang::ASTContext &context, std::string const &restrictToFile)
+    : LexicallyOrderedRecursiveASTVisitor(context.getSourceManager()),
+      restrictToFile(restrictToFile),
+      context(context),
+      sourceManager(context.getSourceManager()),
+      relativeToAbsoluteFilenames(),
+      result()
+    {}
+
+  std::string getStmtFilename(clang::Stmt const *stmt) {
+    auto filename = sourceManager.getFilename(stmt->getBeginLoc()).str();
+    if (relativeToAbsoluteFilenames.find(filename) == relativeToAbsoluteFilenames.end()) {
+      relativeToAbsoluteFilenames[filename] = makeAbsolutePath(filename, sourceManager);
+    }
+    return relativeToAbsoluteFilenames[filename];
+  }
+
+  bool VisitIntegerLiteral(clang::IntegerLiteral *literal) {
+    auto literalLoc = literal->getBeginLoc();
+    if (!literalLoc.isValid()) {
+      return true;
+    }
+
+    std::string inFile = getStmtFilename(literal);
+    if (inFile != restrictToFile) {
+      return true;
+    }
+
+    result.push_back(literal->getValue());
+    return true;
+  }
+};
+
+}
