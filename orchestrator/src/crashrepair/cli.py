c18465be560dab058325f639fb53e70142b489b1
ChrisTimperley <christimperley@googlemail.com>
2022-08-30 09:44:19 -0400

    orchestrator: fixed typo in --no-fuzzing option handling

--- orchestrator/src/crashrepair/cli.py
+++ orchestrator/src/crashrepair/cli.py
@@ -11,7 +11,7 @@ DESCRIPTION = "crashrepair: automated repair of C/C++ security bugs"
 def do_repair(args: argparse.Namespace) -> None:
     scenario = Scenario.for_file(args.filename)
     scenario.should_terminate_early = args.should_terminate_early
-    scenario.skip_fuzzing = args.skip_fuzzing
+    scenario.skip_fuzzing = args.no_fuzzing
     scenario.repair()
 
 
