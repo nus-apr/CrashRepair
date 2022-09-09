8dfdd04accd97938d5c03a685a05262646ba3f97
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-09-09 12:36:23 +0800

    update test case

--- tests/bug-types/buffer-overflow/static-local-array/src/test.c
+++ tests/bug-types/buffer-overflow/static-local-array/src/test.c
@@ -3,8 +3,8 @@
 void bad() {
   int array_0[] = {1234, 3436, 46353};
   int x = 56;
-  int n = 3;
-  int val = array_0[n];
+  int index = 3;
+  int val = array_0[index];
   printf("%d\n", val);
 
 }
