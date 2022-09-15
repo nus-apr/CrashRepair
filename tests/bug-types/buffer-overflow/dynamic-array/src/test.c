a61c102d826de774e8a71f539064d82f9856d121
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-09-15 18:15:56 +0800

    update test-case: change index to an expression

--- tests/bug-types/buffer-overflow/dynamic-array/src/test.c
+++ tests/bug-types/buffer-overflow/dynamic-array/src/test.c
@@ -3,11 +3,13 @@
 void bad() {
   int x = 56;
   int n = 10;
+  int y = 5;
   int arr[n];
   arr[1] = 2;
   arr[0] = 3;
   printf("%d %d %d", arr[0], arr[1], arr[2]);
-  int val = arr[x];
+  int val = 0;
+  val = arr[x+y+5];
 }
 
 int main() {
