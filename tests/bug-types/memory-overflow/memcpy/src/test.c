640a79a32cbbe97011f4141bc2863adfb24f8ae1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-09-22 17:50:54 +0800

    add memcpy test case

---
+++ tests/bug-types/memory-overflow/memcpy/src/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stddef.h>
+
+void copy_zero(int *source, int*target, int size) {
+  memcpy(source, target, size);
+}
+
+int main() {
+  int *source = (int *)malloc(sizeof(int) * 5);
+  memset(source, 0, 5);
+  int *target = (int *)malloc(sizeof(int) * 5);
+  int size = 100;
+  copy_zero(source, target, size);
+}
