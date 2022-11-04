970d97116a09e720b3de1c8c436d4304932ae2bc
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-11-04 17:45:57 +0800

    print ast node for debug purpose

--- app/constraints.py
+++ app/constraints.py
@@ -354,6 +354,7 @@ def generate_expr_for_ast(ast_node)->ConstraintExpression:
         constraint_expr = make_symbolic_expression(constraint_symbol)
         return constraint_expr
     else:
+        print(ast_node)
         utilities.error_exit("Unknown AST node type for Expression: {}".format(node_type))
 
 
