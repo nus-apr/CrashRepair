b815e765ae1f738db6b81f2cbcfacda577c7255a
ChrisTimperley <christimperley@googlemail.com>
2022-11-04 14:44:01 -0400

    repair: added basic test case for #16

--- source-repair/test/parser_test.cpp
+++ source-repair/test/parser_test.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <crashrepairfix/Expr/Parser.h>
+
+std::unique_ptr<crashrepairfix::Expr> parse(std::string const &code) {
+  return crashrepairfix::parse(code);
+}
+
+TEST(ParserTest, HandlesLongMax) {
+  ASSERT_NE(parse("(LONG_MAX < 0)"), nullptr);
 }
