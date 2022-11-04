70cd8d0edcfc16e7408eb98b045b4cbadd95318a
ChrisTimperley <christimperley@googlemail.com>
2022-11-04 15:05:14 -0400

    repair: added regression tests for parse function

--- source-repair/test/parser_test.cpp
+++ source-repair/test/parser_test.cpp
@@ -6,6 +6,14 @@ std::unique_ptr<crashrepairfix::Expr> parse(std::string const &code) {
   return crashrepairfix::parse(code);
 }
 
-TEST(ParserTest, HandlesLongMax) {
-  ASSERT_NE(parse("(LONG_MAX < 0)"), nullptr);
+TEST(ParserTest, VarLessEqConstant) {
+  ASSERT_NE(parse("@var(integer, compinfo->height) <= 7"), nullptr);
+}
+
+TEST(ParserTest, BracketedVarLessEqConstant) {
+  ASSERT_NE(parse("(@var(integer, compinfo->height) <= 7)"), nullptr);
+}
+
+TEST(ParserTest, VarLessEqVar) {
+  ASSERT_NE(parse("@var(integer, compinfo->height) <= @var(integer, cmptparm->prec)"), nullptr);
 }
