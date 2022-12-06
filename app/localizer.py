d0d2a2eb32a88682a3d5b4dd516336919c480e61
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-12-06 18:07:33 +0800

    convert mapping to string

--- app/localizer.py
+++ app/localizer.py
@@ -315,7 +315,7 @@ def localize_cfc(taint_loc, cfc_info, taint_symbolic, taint_concrete):
             else:
                 if "sizeof" in c_t_lookup:
                     concrete_value = cfc_var_info_list[c_t_lookup]["concrete_value"]
-                    localized_tokens[c_t_lookup] = concrete_value
+                    localized_tokens[c_t_lookup] = str(concrete_value)
         logger.track_localization("Localized Tokens {}".format(localized_tokens))
         if len(localized_tokens.keys()) == len(sorted_cfc_tokens):
             localized_cfc = copy.deepcopy(cfc_expr)
