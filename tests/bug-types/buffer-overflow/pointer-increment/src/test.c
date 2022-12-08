a2ca24daace44d9ab2d9d5e87a78ebbc140ad67f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-12-09 00:33:45 +0800

    add new test case

---
+++ tests/bug-types/buffer-overflow/pointer-increment/src/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int x = argv[2];
+  char *path = argv[1];
+  char buffer[10];
+  FILE *fp = fopen(path, "r");
+  fread(buffer, sizeof(int), 1, fp);
+  fclose(fp);
+  int y = buffer[1];
+  int *arr = (int *) malloc(10);
+  int val;
+  int *ptr = arr + x + y;
+  *ptr++ = 12;
+  printf("index %d = %d", x, val);
+}
+
