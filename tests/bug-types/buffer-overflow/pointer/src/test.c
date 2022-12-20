faed5f72573b27bb1a5c2ae8f6a48e293afed024
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-12-20 22:50:57 +0800

    update test case

--- tests/bug-types/buffer-overflow/pointer/src/test.c
+++ tests/bug-types/buffer-overflow/pointer/src/test.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  int x = argv[2];
   char *path = argv[1];
   char buffer[10];
   FILE *fp = fopen(path, "r");
   fread(buffer, sizeof(int), 1, fp);
   fclose(fp);
   int y = buffer[1];
+  int x = buffer[0];
   int *arr = (int *) malloc(10);
   int val;
-  int *ptr = arr + x + y;
+  int *ptr = arr + x;
+  ptr = ptr + y;
   *ptr = 12;
   printf("index %d = %d", x, val);
 }
