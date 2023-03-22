148944a7a8ea1f17d7d3621a7aa0e53df32b4571
ChrisTimperley <christimperley@googlemail.com>
2023-03-21 21:03:40 -0400

    benchmark: removed repair.conf and prebuild/build steps

--- experiments/vulnloc/coreutils/gnubug-19784/setup.sh
+++ experiments/vulnloc/coreutils/gnubug-19784/setup.sh
@@ -3,7 +3,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
-dir_name=/data//$benchmark_name/$project_name/$bug_id
+dir_name=/data/$benchmark_name/$project_name/$bug_id
 current_dir=$PWD
 mkdir -p $dir_name
 cd $dir_name
@@ -21,20 +21,6 @@ git format-patch -1 $fix_commit_id
 cp *.patch $dir_name/dev-patch/fix.patch
 
 ./bootstrap
-FORCE_UNSAFE_CONFIGURE=1 CC=crepair-cc ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
-make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static"
-
-cat <<EOF > $script_dir/repair.conf
-dir_exp:$dir_name
-tag_id:$bug_id
-src_directory:$dir_name/src
-binary_path:$dir_name/src/src/make-prime-list
-config_command:skip
-build_command:make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static" src/make-prime-list
-test_input_list:15
-klee_flags:--link-llvm-lib=/CrashRepair/lib/libcrepair_proxy.bca
-EOF
-
 
 cat <<EOF > $dir_name/bug.json
 {
--- experiments/vulnloc/coreutils/gnubug-25003/setup.sh
+++ experiments/vulnloc/coreutils/gnubug-25003/setup.sh
@@ -3,7 +3,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
-dir_name=/data//$benchmark_name/$project_name/$bug_id
+dir_name=/data/$benchmark_name/$project_name/$bug_id
 current_dir=$PWD
 mkdir -p $dir_name
 cd $dir_name
@@ -20,21 +20,6 @@ git checkout $bug_commit_id
 touch src/a
 
 ./bootstrap
-FORCE_UNSAFE_CONFIGURE=1 CC=wllvm CXX=wllvm++ ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
-make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static"
-
-cat <<EOF > $script_dir/repair.conf
-dir_exp:$dir_name
-tag_id:$bug_id
-src_directory:$dir_name/src
-binary_path:$dir_name/src/src/split
-config_command:skip
-build_command:make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static" src/split
-test_input_list:-n7/75 /dev/null
-klee_flags:--link-llvm-lib=/CrashRepair/lib/libcrepair_proxy.bca
-EOF
-
-
 
 cat <<EOF > $dir_name/bug.json
 {
