cb77295466637520fcd69668845e3f0bd2300123
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-04-02 21:21:53 +0800

    optimization: avoid source locations not part of the subject

--- app/parallel.py
+++ app/parallel.py
@@ -65,10 +65,13 @@ def generate_taint_sink_info(taint_symbolic, taint_memory_list, is_taint_influen
     pool = mp.Pool(values.DEFAULT_CORE_LIMIT, initializer=mute)
     count = 0
     for taint_info in reversed(taint_symbolic.keys()):
+        source_path, line_number, col_number, inst_addr = taint_info.split(":")
+        if "/opt/zlib" in source_path or "/klee-uclibc/" in source_path:
+            continue
         count = count + 1
         if count >= values.DEFAULT_MAX_TAINT_LOCATIONS:
             break
-        source_path, line_number, col_number, inst_addr = taint_info.split(":")
+
         taint_loc = ":".join([source_path, line_number, col_number])
         taint_expr_list = taint_symbolic[taint_info]
         logger.track_localization("Analysing Loc:" + taint_loc)
