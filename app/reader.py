ea1bd73e8de2c2ca0d979bb4c89bf02bad22ef59
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-04-05 20:02:56 +0800

    remove duplicates preserving order

--- app/reader.py
+++ app/reader.py
@@ -464,6 +464,13 @@ def read_tainted_expressions(taint_log_path):
                         data_type = "double"
                     formatted_taint_value = "{}:{}".format(data_type.strip(), taint_value)
                     taint_map[source_loc].append(formatted_taint_value)
+    for taint_loc in taint_map:
+        expr_list = taint_map[taint_loc]
+        unique_var_expr_list = []
+        for v in reversed(expr_list):
+            if v not in unique_var_expr_list:
+                unique_var_expr_list.append(v)
+        taint_map[taint_loc] = unique_var_expr_list
     return taint_map
 
 
