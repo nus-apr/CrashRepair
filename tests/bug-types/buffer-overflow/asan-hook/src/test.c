a454b09007c8ac1e96c782baf74d214c478bce02
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-04-07 01:30:47 +0800

    add new test-case for asan hook

---
+++ tests/bug-types/buffer-overflow/asan-hook/src/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  char *path = argv[1];
+  char buffer[10];
+  FILE *fp = fopen(path, "r");
+  fread(buffer, sizeof(int), 1, fp);
+  fclose(fp);
+  int y = buffer[1];
+  int x = buffer[0];
+  int *arr = (int *) malloc(10);
+  int val;
+  int *ptr = arr;
+  *ptr = 12;
+  printf("index %d = %d", x, val);
+  free(arr);
+}
