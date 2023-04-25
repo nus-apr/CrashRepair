63696e40637ccaadff97f1e2ba4e7d5e063ddde8
ChrisTimperley <christimperley@googlemail.com>
2023-04-25 09:19:43 -0400

    orchestrator: added FuzzerCrashed and FuzzerExhaustedMemory exceptions

--- orchestrator/src/crashrepair/exceptions.py
+++ orchestrator/src/crashrepair/exceptions.py
@@ -13,6 +13,43 @@ class CrashRepairException(Exception):
 
 
 @_attr.s(frozen=True, auto_exc=True, auto_attribs=True)
+class FuzzerExhaustedMemory(CrashRepairException):
+    """Used to indicate that the fuzzer ran out of memory."""
+    def __str__(self) -> str:
+        return "fuzzer failed: ran out of memory"
+
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        return {
+            "kind": "fuzzer-exhausted-memory",
+        }
+
+
+@_attr.s(frozen=True, auto_exc=True, auto_attribs=True)
+class FuzzerCrashed(CrashRepairException):
+    """Used to indicate that the fuzzer crashed.
+
+    Attributes
+    ----------
+    return_code: t.Optional[int]
+        The return code, if any, that was produced by the fuzzer.
+    tail: str
+        Contains the last N lines of the output produced by the fuzzer.
+    """
+    return_code: t.Optional[int]
+    tail: str
+
+    def __str__(self) -> str:
+        return f"fuzzer failed: crashed with return code {self.return_code}"
+
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        return {
+            "kind": "fuzzer-crash",
+            "tail": self.tail,
+            "return-code": self.return_code,
+        }
+
+
+@_attr.s(frozen=True, auto_exc=True, auto_attribs=True)
 class AnalyzerCrashed(CrashRepairException):
     """Used to indicate that the analysis failed to produce a localization file.
 
