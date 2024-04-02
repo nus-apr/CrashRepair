13259c14cd8fe387771d02629ce7528f54d84e26
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-04-02 23:07:50 +0800

    add proxy to longjmp

--- lib/crepair_proxy.c
+++ lib/crepair_proxy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <setjmp.h>
 
 float fabs_crepair(float a);
+void longjmp_crepair( jmp_buf env, int status );
 
 float fabs_crepair(float a){
 
@@ -13,4 +15,8 @@ float fabs_crepair(float a){
 float rint_crepair(float a){
 
   return (int) a;
+}
+
+void longjmp_crepair( jmp_buf env, int status ){
+    return;
 }
